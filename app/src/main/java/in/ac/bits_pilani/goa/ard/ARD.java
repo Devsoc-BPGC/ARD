7b6b4e8328d0be2f49e61fa4e74312ef1b3f0a8c
Vikramaditya Kukreja <kukreja.vikramaditya@gmail.com>
2017-05-17 17:28:45 +0530

    Issue #21: Add Realm and minor cleanup

--- app/src/main/java/in/ac/bits_pilani/goa/ard/ARD.java
+++ app/src/main/java/in/ac/bits_pilani/goa/ard/ARD.java
@@ -3,8 +3,13 @@ package in.ac.bits_pilani.goa.ard;
 import android.app.Application;
 import android.content.Context;
 
+import in.ac.bits_pilani.goa.ard.utils.AHC;
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
 /**
  * Custom class extending the Application class.
+ * This is used for setting up libraries.
  * @author vikramaditya
  */
 
@@ -13,6 +18,12 @@ public class ARD extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Realm.init(this);
+        RealmConfiguration realmConfiguration = new RealmConfiguration.Builder()
+                .name(AHC.REALM_ARD_DATABASE)
+                .schemaVersion(AHC.REALM_ARD_DATABASE_SCHEMA)
+                .build();
+        Realm.setDefaultConfiguration(realmConfiguration);
     }
 
     @Override
--- app/src/main/java/in/ac/bits_pilani/goa/ard/utils/AHC.java
+++ app/src/main/java/in/ac/bits_pilani/goa/ard/utils/AHC.java
@@ -12,19 +12,22 @@ public class AHC {
     }
 
     /**
-     * Default separator to use.
-     */
-    public static final String SEPARATOR = ".";
-
-    /**
      * Package name of project.
      */
-    public static final String PACKAGE_NAME = "in" + SEPARATOR + "ac" + SEPARATOR
-            + SEPARATOR + "bits_pilani" + SEPARATOR + "goa" + SEPARATOR + "ard";
+    public static final String PACKAGE_NAME = "in.ac.bits_pilani.goa.ard";
 
     /**
-     * default Log tag for project.
+     * Default Log tag for project.
      */
+    public static final String TAG = "ard.bits.goa";
 
-    public static final String TAG = "ard" + SEPARATOR + "bits"  + SEPARATOR + "goa";
+    /**
+     * Name of Realm database.
+     */
+    public static final String REALM_ARD_DATABASE = "REALM_ARD_DATABASE";
+
+    /**
+     * Version of Realm database.
+     */
+    public static final int REALM_ARD_DATABASE_SCHEMA = 0;
 }
