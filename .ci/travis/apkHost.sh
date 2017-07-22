8ceafb8344db0dc96d77504e97b38dbadaaae867
Vikramaditya Kukreja <kukreja-vikramaditya@users.noreply.github.com>
2017-07-23 00:25:04 +0530

    Issue #3: Add script to post apk of builds to server (#136)

---
+++ .ci/travis/apkHost.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -i -X POST $SERVER_POST -H "Content-Type: application/vnd.android.package-archive" -H "Project-Name: ARD" -H "Sub-Directory: $TRAVIS_BRANCH" -H "Extra-Data: $TRAVIS_EVENT_TYPE--PR-$TRAVIS_PULL_REQUEST--Travis-Build-$TRAVIS_BUILD_NUMBER" --data-binary "@app/build/outputs/apk/app-debug.apk" -v
