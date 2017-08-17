36d1141effd79a1e8525e1944fb1a032333d51c2
Vikramaditya Kukreja <kukreja-vikramaditya@users.noreply.github.com>
2017-08-17 12:22:18 +0530

    Issue #105: Add API level 22 for testing (#140)

---
+++ .ci/travis/startEmulator.sh
@@ -0,0 +1,10 @@
+echo no| android create avd --force -n test -t android-$EMULATOR_API
+${ANDROID_HOME}/tools/emulator -avd test -no-skin -no-audio -no-window -netspeed full -netdelay none &
+android-wait-for-emulator
+adb shell input keyevent 82 &
+adb shell settings put global window_animation_scale 0 &
+adb shell settings put global transition_animation_scale 0 &
+adb shell settings put global animator_duration_scale 0 &
+mkdir ./app/debug/logcat -p
+adb logcat -c
+adb logcat > ./app/debug/logcat/logcat &
